b6cd6f415b495ed8cc673b4f1cee470cb4564f15
Vincent Pierce <vinhpierce@gmail.com>
2024-09-12 23:04:01 -0700

    new input

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,7 @@ int main(int argc, char* argv[]) {
     int c;
     FILE *inFile = stdin;
     FILE *outFile = stdout;
-    printf("%s\n%s\n", argv[0], argv[1]); // argv[0] = myExecutable argv[1] = null
     inFile = safe_open(argv[1]);
-    printf("delete me\n");
     char* oldLine, *newLine;
     oldLine = read_long_line(inFile); /* first line */
     fputs(oldLine, outFile);          /* or use puts(oldLine) automatically opens stream stdout */
